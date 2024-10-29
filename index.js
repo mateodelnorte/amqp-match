31fc7afcc8efd1a3710233df617544afad360e8c
Maxence <max@23.tf>
2024-10-29 13:46:12 -0400

    escape dots and handle multi word wildcards properly

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 module.exports = function match (actual, expected) {
-  if (actual === expected) return true;
-  var regexString = '^' + expected.replace(/\*/g, '([^.]+)').replace(/#/g, '([^.]+\.?)+') + '$';
-  return actual.search(regexString) !== -1;
+	if (actual === expected) return true;
+	var regexString = '^' +
+		expected.replace(/[.]/g, '\\.') // escape dots
+			.replace(/\*/g, '([^.]+)') // single word wildcard
+			.replace(/^#\\./, '([^.]+[.])*') // multi word wildcard at start of key
+			.replace(/\\.#$/, '([.][^.]+)*') // multi word wildcard at end of key
+			.replace(/\\.#\\./g, '(([.].*[.])*|[.])') // multi word wildcard within key
+			+ '$';
+	return actual.search(regexString) !== -1;
 };
-
