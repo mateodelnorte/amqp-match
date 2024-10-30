b4e4ecca836278bb19588165e8aa694610d769bd
Max <github@nbr23.com>
2024-10-30 19:21:06 -0400

    Fix multi-word wildcard handling + add a bunch of test cases (#4)
    * escape dots and handle multi word wildcards properly

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
-/*jslint node: true */
+/* jslint node: true */
+/* eslint-env mocha */
 'use strict';
 
 const should = require('should');
@@ -7,34 +8,139 @@ let match = require('../');
 
 describe('amqp-match', () => {
 
-  it('should match this.key to this.key (direct equality)', () => {
+	// Constant
+	it('should match this.key to this.key (direct equality)', () => {
 
-    match('this.key', 'this.key').should.eql(true);
+		match('this.key', 'this.key').should.eql(true);
 
-  });
+	});
+	it('should not match key to this.key (direct inequality)', () => {
 
-  it('should match this.new.key to this.*.key (single word wildcard)', () => {
+		match('key', 'this.key').should.eql(false);
 
-    match('this.new.key', 'this.*.key').should.eql(true);
+	});
 
-  });
+	// Single word wildcard
+	it('should match this.new.key to this.*.key (single word wildcard)', () => {
 
-  it('should not match this.new.other.key to this.*.key (single word wildcard)', () => {
+		match('this.new.key', 'this.*.key').should.eql(true);
 
-    match('this.new.other.key', 'this.*.key').should.not.eql(true);
+	});
 
-  });
+	it('should match this.new.key to *.new.key (beginning single word wildcard)', () => {
 
-  it('should match this.new.kinda.key to this.#.key (multi word wildcard)', () => {
+		match('this.new.key', '*.new.key').should.eql(true);
 
-    match('this.new.kinda.key', 'this.#.key').should.eql(true);
+	});
 
-  });
+	it('should match this.new.key to this.new.* (end single word wildcard)', () => {
 
-  it('should not match some.new.kinda.key to this.#.key (single word wildcard)', () => {
+		match('this.new.key', 'this.new.*').should.eql(true);
 
-    match('some.new.kinda.key', 'this.#.key').should.not.eql(true);
+	});
 
-  });
+	it('should not match this-new.key to this.*.key (single word wildcard)', () => {
 
+		match('this-new.key', 'this.*.key').should.eql(false);
+
+	});
+
+	it('should not match some.new.kinda.key to this.*.key (single word wildcard)', () => {
+
+		match('some.new.kinda.key', 'this.*.key').should.eql(false);
+
+	});
+
+	it('should not match this.new.key.end to this.new.* (end single word wildcard)', () => {
+
+		match('this.new.key.end', 'this.new.*').should.eql(false);
+
+	});
+
+	it('should not match start.this.new.key to *.new.key (beginning single word wildcard)', () => {
+
+		match('start.this.new.key', '*.new.key').should.eql(false);
+
+	});
+
+	it('should not match this.new.key to *.* (single word wildcards)', () => {
+		
+		match('this.new.key', '*.*').should.eql(false);
+	});
+
+	it('should match this.key to *.* (single word wildcards)', () => {
+		
+		match('this.key', '*.*').should.eql(true);
+	});
+
+	// Multi word wildcard
+
+	it('should match this.new.kinda.key to this.#.key (multi word wildcard)', () => {
+
+		match('this.new.kinda.key', 'this.#.key').should.eql(true);
+
+	});
+
+	it('should match this.key to this.#.key (multi word wildcard with empty matching)', () => {
+
+		match('this.key', 'this.#.key').should.eql(true);
+
+	});
+
+	it('should match this.kind.of.key.end to this.kind.of.# (end of key wildcard)', () => {
+
+		match('this.kind.of.key.end', 'this.kind.of.#').should.eql(true);
+	});
+
+	it('should match this.kind.of to this.kind.of.# (end of key wildcard without end)', () => {
+
+		match('this.kind.of', 'this.kind.of.#').should.eql(true);
+	});
+
+	it('should match this.kind.of.key.end to this.#.key.# (multi wildcard with end of key wildcard)', () => {
+
+		match('this.kind.of.key.end', 'this.#.key.#').should.eql(true);
+	});
+
+	it('should match this.kind.of.key.end to this.#.key.# (multi wildcard with end of key wildcard and no end of key)', () => {
+
+		match('this.kind.of.key', 'this.#.key.#').should.eql(true);
+	});
+
+	it('should not match wildcard with bad constant parts', () => {
+
+		match('this.kind.of.notkey', 'this.#.key.#').should.eql(false);
+	});
+
+	it('should match multi word wildcard at beginning of string', () => {
+
+		match('this.kind.of.key', '#.key').should.eql(true);
+	});
+
+	it('should not match multi word wildcard at beginning of string with bad constant part', () => {
+
+		match('this.kind.of.notkey', '#.key').should.eql(false);
+	});
+
+	// Mixed
+
+	it('should work on mixed keys', () => {
+
+		match('this.key1.of.key2', '#.key1.*.key2').should.eql(true);
+	});
+
+	it('should fail on bad mixed keys 1', () => {
+
+		match('bad.this.key1.of.key2', 'good.#.key1.*.key2').should.eql(false);
+	});
+
+	it('should fail on bad mixed keys 2', () => {
+
+		match('bad.this.key1.of.key2.end', '#.key1.*.key2').should.eql(false);
+	});
+
+	it('should fail on bad mixed keys 3', () => {
+
+		match('bad.this.key1.of.key2.end', '#.key1.*.key2.*.*').should.eql(false);
+	});
 });
