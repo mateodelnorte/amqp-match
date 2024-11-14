f02b6c02708a7c8ff65ecd7403ec6bda94e89ebc
Maxence <max@23.tf>
2024-11-14 16:25:08 -0500

    fix '#' handling

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = function match (actual, expected) {
 	var regexString = '^' +
 		expected.replace(/[.]/g, '\\.') // escape dots
 			.replace(/\*/g, '([^.]+)') // single word wildcard
+			.replace(/^#$/, '([^.]+[.]?)*') // multi word wildcard on its own
 			.replace(/^#\\./, '([^.]+[.])*') // multi word wildcard at start of key
 			.replace(/\\.#$/, '([.][^.]+)*') // multi word wildcard at end of key
 			.replace(/\\.#\\./g, '(([.].*[.])*|[.])') // multi word wildcard within key
